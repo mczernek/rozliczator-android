ae2f1038edf731cfc5b1ba0d3f66f86e265883e4
mczernek <czernekmichal@gmail.com>
2015-01-09 16:30:55 +0100

    Preparing build with gradle and collapse animation added

--- src/pl/kap11/rozliczator/MainActivity.java
+++ src/pl/kap11/rozliczator/MainActivity.java
@@ -1,10 +1,15 @@
 package pl.kap11.rozliczator;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 
 import pl.kap11.rozliczator.event.EventsFragment;
 
@@ -30,13 +35,56 @@ public class MainActivity extends Activity {
         bottomButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bottomFragment.setVisibility(View.VISIBLE);
-                mainFragmentContainer.setVisibility(View.GONE);
+
+                if(bottomFragment.getVisibility() != View.VISIBLE) {
+                    expandBottomFragment();
+                }else{
+                    collapseBottomFragment();
+                }
             }
         });
 
-
 	}
-	
-	
+
+    private void collapseBottomFragment() {
+        ValueAnimator collapse = new ValueAnimator();
+        collapse.setInterpolator(new AccelerateDecelerateInterpolator());
+        collapse.setDuration(200);
+        int viewHeight = bottomFragment.getMeasuredHeight();
+        collapse.setIntValues(viewHeight, 0);
+        collapse.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                Integer value = (Integer) animation.getAnimatedValue();
+                bottomFragment.getLayoutParams().height = value.intValue();
+                bottomFragment.requestLayout();
+            }
+        });
+        collapse.addListener( new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {}
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                bottomFragment.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                bottomFragment.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {}
+        });
+        collapse.start();
+    }
+
+    private void expandBottomFragment() {
+        ScaleAnimation expand = new ScaleAnimation(1,1,0,1);
+        expand.setDuration(200);
+        bottomFragment.startAnimation(expand);
+    }
+
+
 }
