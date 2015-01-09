38992e33475fbeeb7d62775c22b21162015076d0
mczernek <czernekmichal@gmail.com>
2015-01-09 15:06:54 +0100

    Animation

--- src/pl/kap11/rozliczator/MainActivity.java
+++ src/pl/kap11/rozliczator/MainActivity.java
@@ -1,23 +1,41 @@
 package pl.kap11.rozliczator;
 
-import pl.kap11.rozliczator.event.EventsFragment;
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.view.View;
+
+import pl.kap11.rozliczator.event.EventsFragment;
 
 public class MainActivity extends Activity {
+
+    View bottomButton;
+    View bottomFragment;
+    View mainFragmentContainer;
+    Fragment mainFragment;
 
 	@Override
 	public void onCreate(final Bundle savedState){
 		super.onCreate(savedState);
 		setContentView(R.layout.activity_main);
-		
-		Fragment fragment = new EventsFragment();
-		
-//		FragmentManager manager = getFragmentManager();
-//		FragmentTransaction transaction = manager.beginTransaction();
-//		transaction.add(R.id.main_fragment, fragment);
-//		transaction.commit();
+
+		FragmentManager manager = getFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.main_fragment);
+
+        bottomButton = findViewById(R.id.bottom_buttons);
+        mainFragmentContainer = findViewById(R.id.main_fragment_container);
+        bottomFragment = findViewById(R.id.bottom_fragment);
+
+        bottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomFragment.setVisibility(View.VISIBLE);
+                mainFragmentContainer.setVisibility(View.GONE);
+            }
+        });
+
+
 	}
 	
 	
