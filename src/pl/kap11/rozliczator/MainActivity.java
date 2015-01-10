0aec089e8eebb86c29557b1d92c568651da2ddc1
mczernek <czernekmichal@gmail.com>
2015-01-10 02:13:09 +0100

    Added darkening of list while expanding bottom view, some refactoring

--- src/pl/kap11/rozliczator/MainActivity.java
+++ src/pl/kap11/rozliczator/MainActivity.java
@@ -1,26 +1,25 @@
 package pl.kap11.rozliczator;
 
-import android.animation.Animator;
-import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.Animation;
-import android.view.animation.ScaleAnimation;
 
-import pl.kap11.rozliczator.event.EventsFragment;
+import pl.kap11.rozliczator.animation.ExpandableOnClickView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements ExpandableOnClickView.VisibilityListener{
 
     View bottomButton;
     View bottomFragment;
     View mainFragmentContainer;
     Fragment mainFragment;
+    View mainFragmentOverlay;
 
-	@Override
+    ExpandableOnClickView expandableView;
+
+    @Override
 	public void onCreate(final Bundle savedState){
 		super.onCreate(savedState);
 		setContentView(R.layout.activity_main);
@@ -31,60 +30,46 @@ public class MainActivity extends Activity {
         bottomButton = findViewById(R.id.bottom_buttons);
         mainFragmentContainer = findViewById(R.id.main_fragment_container);
         bottomFragment = findViewById(R.id.bottom_fragment);
+        mainFragmentOverlay = findViewById(R.id.man_fragment_overlay);
 
-        bottomButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if(bottomFragment.getVisibility() != View.VISIBLE) {
-                    expandBottomFragment();
-                }else{
-                    collapseBottomFragment();
-                }
-            }
-        });
+        expandableView = new ExpandableOnClickView(bottomButton, bottomFragment, ExpandableOnClickView.State.COLLAPSED);
+        expandableView.addSizeListener(this);
 
 	}
 
-    private void collapseBottomFragment() {
-        ValueAnimator collapse = new ValueAnimator();
-        collapse.setInterpolator(new AccelerateDecelerateInterpolator());
-        collapse.setDuration(200);
-        int viewHeight = bottomFragment.getMeasuredHeight();
-        collapse.setIntValues(viewHeight, 0);
-        collapse.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                Integer value = (Integer) animation.getAnimatedValue();
-                bottomFragment.getLayoutParams().height = value.intValue();
-                bottomFragment.requestLayout();
-            }
-        });
-        collapse.addListener( new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {}
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                bottomFragment.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-                bottomFragment.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {}
-        });
-        collapse.start();
-    }
-
-    private void expandBottomFragment() {
-        ScaleAnimation expand = new ScaleAnimation(1,1,0,1);
-        expand.setDuration(200);
-        bottomFragment.startAnimation(expand);
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        expandableView.removeSizeListener(this);
     }
 
 
+    @Override
+    public void onExpansionStarted() {
+        mainFragmentOverlay.setVisibility(View.VISIBLE);
+        mainFragmentContainer.setClickable(false);
+    }
+
+    @Override
+    public void onCollapseStarted() {
+        mainFragmentOverlay.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onViewCollapsed() {
+        mainFragmentOverlay.setVisibility(View.GONE);
+        mainFragmentContainer.setClickable(true);
+    }
+
+    @Override
+    public void onViewExpanded() {
+        mainFragmentOverlay.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onViewSizeUpdated(float fraction) {
+        float maxAlpha = 0.75f;
+        Log.d("FLOAT", "Overlay alpha " + fraction);
+        mainFragmentOverlay.setAlpha(fraction * maxAlpha / 100);
+    }
 }
